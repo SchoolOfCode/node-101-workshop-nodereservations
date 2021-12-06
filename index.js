28a18e32ae54065a1a4966bef066e60c2a3a978e
ryanglennsmith <ryan.glenn.smith@gmail.com>
2021-12-06 16:55:02 +0000

    finished main tasks

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ import chalk from "chalk";
 function describeItem(item) {
   if (item.count === 1) {
     console.log(
-      `I have a ${chalk.cyan(item.name)}. Here's what I like about it: ${
-        item.whatILike
-      }.`
+      `I have a ${chalk.cyan(
+        item.name
+      )}. Here's what I like about it: ${chalk.green(item.whatILike)}.`
     );
   } else {
     console.log(
-      `I have ${item.count} ${item.name}s. Here's what I like about them: ${item.whatILike}.`
+      `I have ${chalk.yellow(item.count)} ${chalk.cyan(
+        item.name
+      )}s. Here's what I like about them: ${chalk.green(item.whatILike)}.`
     );
   }
-  console.log(item.count);
+  //   console.log(item.count);
 }
 function describeCollection(arr) {
   arr.forEach(describeItem);
